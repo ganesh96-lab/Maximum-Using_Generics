5365f21332d9b8609ee5fceb1c9421093b856b9a
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-04 02:58:49 -0400

    ADDED:Given three integers find maximum

---
+++ src/main/java/com/bridgelabz/maxanalyser/MaximumAnalyser.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.maxanalyser;
+
+public class MaximumAnalyser {
+    public Integer findMaximum(Integer firstNumber,Integer secondNumber,Integer thirdNumber) {
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+            return firstNumber;
+        }
+        else if (secondNumber.compareTo(firstNumber) > 0 && secondNumber.compareTo(thirdNumber) > 0) {
+            return secondNumber;
+        }
+        else {
+            return thirdNumber;
+        }
+    }
+
+    public Float findMaximum(Float firstNumber,Float secondNumber,Float thirdNumber){
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0){
+            return firstNumber;
+        }
+        else if (secondNumber.compareTo(firstNumber) > 0 && secondNumber.compareTo(thirdNumber) > 0){
+            return secondNumber;
+        }
+        else {
+            return thirdNumber;
+        }
+    }
+}
---
+++ src/test/java/com/bridgelabz/maxanalyser/MaximumAnalyserTest.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.maxanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumAnalyserTest
+{
+    MaximumAnalyser maximumAnalyser=new MaximumAnalyser();
+
+    @Test
+    public void givenThreeIntegers_findMax_atFirstPosition() {
+        Integer maximum=maximumAnalyser.findMaximum(10,20,30);
+        Assert.assertEquals((Integer)30,maximum);
+    }
+
+    @Test
+    public void givenThreeIntegers_findMax_atSecondPosition() {
+        Integer maximum=maximumAnalyser.findMaximum(10,20,30);
+        Assert.assertEquals((Integer)30,maximum);
+    }
+
+    @Test
+    public void givenThreeIntegers_findMax_atThirdPosition() {
+        Integer maximum =maximumAnalyser.findMaximum(10,20,30);
+        Assert.assertEquals((Integer)30,maximum);
+    }
+
+    @Test
+    public void givenThreeFloat_findMax_atFirstPosition() {
+        Float maximum=maximumAnalyser.findMaximum(10.1f,20.2f,30.3f);
+        Assert.assertEquals((Float)30.3f,maximum);
+    }
+
+    @Test
+    public void givenThreeFloat_findMax_atSecondPosition(){
+        Float maximum =maximumAnalyser.findMaximum(10.1f,20.2f,30.3f);
+        Assert.assertEquals((Float)30.3f,maximum);
+    }
+
+    @Test
+    public void givenThreeFloat_findMax_atThirdPosition() {
+        Float maximum=maximumAnalyser.findMaximum(10.1f,20.2f,30.3f);
+        Assert.assertEquals((Float)30.3f,maximum);
+    }
+}
